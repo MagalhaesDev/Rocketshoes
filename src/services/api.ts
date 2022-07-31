e6419782af49a8274ed33c7cf4d6a217a8a48135
Mateus Magalhães <95831951+MagalhaesDev@users.noreply.github.com>
2022-07-31 19:45:11 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-  baseURL: 'https://rocket-shoes-omega.vercel.app/api'
+  baseURL: 'https://rocket-shoes-ignite-eight.vercel.app'
 })
 
 
