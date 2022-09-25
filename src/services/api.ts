df029a405da5d3debe135153094528261423004a
Mateus Magalhães <95831951+MagalhaesDev@users.noreply.github.com>
2022-09-25 19:29:47 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-  baseURL: 'https://rocket-shoes-ignite-eight.vercel.app/api'
+  baseURL: 'https://my-json-server.typicode.com/MagalhaesDev/Criando-um-hook-de-carrinhos-de-compras'
 })
 
 
